d87e570c4508d3b2ed88f8d1a4e0881befc5757a
Muhammad Moeen <mooenmalik374@gmail.com>
2026-03-05 20:31:49 +0500

    Configured the App context store to access the store data in any component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { Link, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { useState } from "react";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
-  const [user, setUser] = useState(null);
   const navigate = useNavigate();
+  const { user } = useContext(AppContext);
   return (
     <div className="flex justify-between items-center py-4">
       <Link to="/">
@@ -13,7 +14,7 @@ const Navbar = () => {
       <div>
         {user ? (
           <div className="flex items-center gap-2 sm:gap-3 ">
-            <button className="flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700">
+            <button className="flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700" onClick={()=>navigate("/buy")}>
               <img src={assets.credit_star} alt="" className="w-5" />
               <p className="text-xs sm:text-sm font-medium text-gray-600">
                 Credits left : 50
