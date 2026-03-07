9b0c151fdd6fa7dedade2d9930248bd39e6fdb4e
Muhammad Moeen <mooenmalik374@gmail.com>
2026-03-07 13:25:42 +0500

    Added the animation in the buy credit page.

--- src/pages/BuyCredit.jsx
+++ src/pages/BuyCredit.jsx
@@ -1,11 +1,18 @@
 import { useContext } from "react";
 import { assets, plans } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
+import { motion } from "motion/react";
 
 const BuyCredit = () => {
   const { user } = useContext(AppContext);
   return (
-    <div className="min-h-[80vh] text-center pt-14 mb-10">
+    <motion.div
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      className="min-h-[80vh] text-center pt-14 mb-10"
+    >
       <button className="border border-gray-400 px-10 py-2 rounded-full mb-6">
         Our Plans
       </button>
@@ -16,9 +23,12 @@ const BuyCredit = () => {
       {/* Plan Cards */}
       <div className="flex flex-wrap justify-center gap-6 text-left">
         {plans.map((plan, index) => (
-          <div
+          <motion.div
+            initial={{ opacity: 0.2, y: 20 }}
+            transition={{ delay: 0.1, duration: 1 }}
+            whileInView={{ opacity: 1, y: 0 }}
             key={index}
-            className="bg-white drop-shadow-sm border rounded-lg py-12 px-8 text-gray-600 hover:scale-105 transition-all duration-500"
+            className={`bg-white drop-shadow-sm border rounded-lg py-12 px-8 text-gray-600 hover:scale-105 transition-all duration-500 ${index === 1 && "md:scale-110 hover:scale-110"}`}
           >
             <img width={40} src={assets.logo_icon} alt="" />
             <p className="mt-3 mb-1 font-semibold">{plan.id}</p>
@@ -30,10 +40,10 @@ const BuyCredit = () => {
             <button className="w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2.5 min-w-52">
               {user ? "Purchase" : "Get Started"}
             </button>
-          </div>
+          </motion.div>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
